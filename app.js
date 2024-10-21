90ff5a09195e82ed6f4b1d935ac765f478bb6100
ssheikhi <fx.sheikhi@gmail.com>
2024-10-21 13:01:13 +0200

    Exercise 9: Joining elements

--- app.js
+++ app.js
@@ -83,3 +83,33 @@ Complete Exercise 7 in the space below:
 let yummy = foods.slice(1,3);
 console.log('Exercise 7 result:', yummy);
 
+/*
+Exercise 8: Finding an index
+
+1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+   `foods` array. 
+
+2) Assign it to a variable named `soyIdx`.
+
+Complete Exercise 8 in the space below:
+*/
+let soyIdx = foods.indexOf('tofu');
+console.log('Exercise 8 result:', soyIdx);
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+
+let allFoods = foods.join(' -> ');
+
+console.log('Exercise 9 result:', allFoods);
